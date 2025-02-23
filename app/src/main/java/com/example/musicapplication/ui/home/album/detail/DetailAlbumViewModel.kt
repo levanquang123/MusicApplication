d29d9a970163f6fbb66132e5094e5ff2151cdde3
levanquang123 <levanquang27122005@gmail.com>
2025-02-23 16:34:22 +0700

    designed detail album

---
+++ app/src/main/java/com/example/musicapplication/ui/home/album/detail/DetailAlbumViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.musicapplication.ui.home.album.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailAlbumViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

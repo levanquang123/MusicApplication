17811ed5f27752b345679801ef79faff479008df
levanquang123 <levanquang27122005@gmail.com>
2025-05-10 16:58:45 +0700

    designed more playlist

---
+++ app/src/main/java/com/example/musicapplication/ui/library/playlist/more/MorePlaylistViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.musicapplication.ui.library.playlist.more
+
+import androidx.lifecycle.ViewModel
+
+class MorePlaylistViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
